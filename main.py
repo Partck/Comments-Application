93353affc283b3c14f5ab3bd39519ac7b34ddc70
ipaullly <ipaully22@gmail.com>
2018-11-19 13:19:27 +0300

    [finishes #162044431] add options for actions

--- main.py
+++ main.py
@@ -14,13 +14,26 @@ def main():
             print("User must have a password")
             break
         
-        time.sleep(5)
+        time.sleep(0.8)
         greeting = 'Hello, {}! Welcome to Comments-Agile. please login to begin creating posts'.format(user)
     
         print(greeting)
 
         log_user = input('Please enter your account name:')
         log_pass = input('Enter your account password:')
+
+        time.sleep(0.8)
+        print("\n")
+        print("1. Create a new comment")
+        print("2. Edit comment")
+        print("3. Delete comment")
+        print("4. Reply to a new comment")
+        print("\n")
+        
+        choice = input("Enter choice of action:")
+        if choice != int:
+            print("Choice must be numeral")
+            break
 
         comment = input('enter new comment:')
 
