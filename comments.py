8a2a18ad1e6762a6343e1414da03107b1974122c
Harrison-Gitau <gitau.h.mwangi@gmail.com>
2018-11-19 13:04:40 +0300

    [Feature #162043831] Create new comment function

---
+++ comments.py
@@ -0,0 +1,20 @@
+comments = []
+
+class Comments:
+
+	def __init__(self):
+		self.db = comments
+
+
+	def create_comments(self, message, author):
+
+		payload = {
+			"id": len(self.db)+1,
+			"message": message,
+			"timestamp": timestamp,
+			"author": author
+		}
+		self.db.append(payload)
+		return payload
+
+	
